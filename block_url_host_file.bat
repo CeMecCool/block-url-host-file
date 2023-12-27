d2fc62a069eb39a0cee2aa2ae3d3cc022a2f1ab3
CeMecCool <149938076+CeMecCool@users.noreply.github.com>
2023-12-27 15:54:31 +0100

    Create block_url_host_file.bat

---
+++ block_url_host_file.bat
@@ -0,0 +1,24 @@
+@echo off
+TITLE Block URL Host File
+CHCP 65001
+CLS
+
+:check_Permissions 
+    net session >nul 2>&1
+    IF %errorLevel% == 0 (
+	ECHO Block URL Host File
+    ) ELSE (
+        ECHO Faut lancer le script en tant que administrateur
+	PAUSE
+    )
+
+:seturl
+ECHO.
+SET /P URL="URL: "
+ECHO 0.0.0.0       %URL% >> "C:\Windows\System32\drivers\etc\hosts"
+ECHO %URL% ajoutée au fichier host
+ECHO.
+
+choice /C:YN /M:"Continuer? "
+IF ERRORLEVEL ==1 GOTO seturl
+IF ERRORLEVEL ==2 EXIT
